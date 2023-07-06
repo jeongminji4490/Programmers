bed3e4c593fc1f95aca3545a49040fd0d5796253
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-07-06 19:25:20 +0900

    [unrated] Title: 문자열 묶기, Time: 13.16 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.kt
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.kt
@@ -1,14 +1,3 @@
 class Solution {
-    fun solution(strArr: Array<String>): Int {
-
-        val arr = strArr.groupBy { it.length }
-        val values = arr.values
-        var newArr = intArrayOf()
-        
-        values.forEach {
-            newArr += it.size
-        }
-
-        return newArr.maxOf {it}
-    }
+    fun solution(strArr: Array<String>) = strArr.groupBy { it.length }.maxOf { (k, v) -> v.size }
 }
