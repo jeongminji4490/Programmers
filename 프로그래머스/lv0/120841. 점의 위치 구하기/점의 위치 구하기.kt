49429b96f9fd7fb3462e59a2ec7550cc51c4c85b
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-07-24 10:34:11 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(dot: IntArray): Int {
+        val x = dot[0]
+        val y = dot[1]
+        if (x>=0 && y>=0) {
+            return 1
+        } else if (x<0 && y>=0) {
+            return 2
+        } else if (x<0 && y<0) {
+            return 3
+        } else return 4
+    }
+}
