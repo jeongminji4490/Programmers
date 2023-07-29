3c23fc52804e75dc708610672d082873a0d3e47d
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-07-29 10:31:04 +0900

    [level 0] Title: 중앙값 구하기, Time: 19.45 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.kt
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.kt
@@ -1,6 +1,5 @@
 class Solution {
     fun solution(array: IntArray): Int {
-        var answer: Int = 0
         val _array = array.toList().sorted()
         val median_idx = (_array.size-1) / 2
         return _array[median_idx]
