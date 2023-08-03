ae8e18d3e50ca41ab5536e2cf081f65ccc3be407
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-08-03 17:09:12 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(array: IntArray, height: Int): Int {
+        var answer: Int = 0
+        array.forEach { 
+            if (it > height) {
+                answer++
+            }
+        }
+        return answer
+    }
+}
