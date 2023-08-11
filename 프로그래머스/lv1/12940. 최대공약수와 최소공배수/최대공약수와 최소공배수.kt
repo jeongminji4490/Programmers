d8c93bb4620607d2ca50694a9ecdec6c006c2314
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-08-11 20:12:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 9.96 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(n: Int, m: Int): IntArray {
+        var answer = intArrayOf()
+        answer += gcd(n, m)
+        answer += (n * m) / gcd(n, m)
+        return answer
+    }
+    
+    fun gcd(a: Int, b:Int): Int = if(b != 0) gcd(b, a % b) else a
+}
