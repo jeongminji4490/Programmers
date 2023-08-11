fd5e3fe5523477a81be9f32a2e829f9d7f2eec44
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-08-11 11:45:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 18.99 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(numbers: IntArray): Int {
-        val list = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-        val result = list.filterNot { numbers.contains(it) }
-        return result.sum()
+        return 45 - numbers.sum()
     }
 }
