5bc7e3358328d6b614297813584e11d91527c438
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-08-28 17:57:15 +0900

    [level 1] Title: 모의고사, Time: 19.10 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.kt
@@ -1,12 +1,13 @@
 class Solution {
     fun solution(answers: IntArray): IntArray {
         var answer = intArrayOf()
-        var first = listOf(1, 2, 3, 4, 5)
-        var second = listOf(2, 1, 2, 3, 2, 4, 2, 5)
-        var third = listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
-    
+        
+        val first = intArrayOf(1, 2, 3, 4, 5)
+        val second = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
+        val third = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        
         var scores = arrayListOf(0, 0, 0)
-    
+        
         for ((idx, value) in answers.withIndex()) {
             if (value == first[idx%5]) {
                 scores[0] += 1
@@ -18,12 +19,12 @@ class Solution {
                 scores[2] += 1
             }
         }
-    
+        
         val max = scores.maxOf { it }
-    
+        
         for ((idx, value) in scores.withIndex()) {
             if (value == max) {
-                answer += (idx+1)
+                answer += (idx + 1)
             }
         }
     
