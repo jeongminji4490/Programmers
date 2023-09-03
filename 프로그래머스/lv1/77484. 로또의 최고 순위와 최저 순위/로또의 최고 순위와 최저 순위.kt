5c3f7a4b4caa6f81c0e68095c37c09ca841f9753
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-09-03 22:39:32 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 14.14 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+        // var answer: IntArray = intArrayOf()
+        val zero_count = lottos.filter { it == 0 }.count()
+        val same_count = lottos.filter { win_nums.contains(it) }.count()
+  
+        val max_score = zero_count + same_count
+        val min_score = same_count
+        
+        val max_ranking = getRanking(max_score)
+        val min_ranking = getRanking(min_score)
+        
+        return intArrayOf(max_ranking, min_ranking)
+    }
+    
+    fun getRanking(score: Int): Int {
+        return when (score) {
+            6 -> 1
+            5 -> 2
+            4 -> 3
+            3 -> 4
+            2 -> 5
+            else -> 6
+        }
+    }
+}
