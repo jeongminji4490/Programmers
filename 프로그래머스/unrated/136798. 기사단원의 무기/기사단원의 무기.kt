fd3268c875a21a472fdf783d200404734390cdab
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-09-03 22:25:57 +0900

    [unrated] Title: 기사단원의 무기, Time: 6.90 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.kt
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.kt
@@ -2,23 +2,23 @@ import kotlin.math.*
 
 class Solution {
     fun solution(number: Int, limit: Int, power: Int): Int {
-        var divisorList = arrayListOf<Int>()
-        var divisorNumList = arrayListOf<Int>() 
-    
+        val divisorList = mutableListOf<Int>()
+        val divisorNumList = mutableListOf<Int>()
+        
         for (i in 1..number) {
-            val sqrt = sqrt(i.toDouble())
-            for (j in 1 .. sqrt.toInt()) {
-        	    if (i % j == 0) {
-            	    divisorList.add(j)
-            	    if (i / j != j) {
-                	    divisorList.add(i / j)
-            	    }
-        	    }
+            val sq = sqrt(i.toDouble())
+            for (j in 1..sq.toInt()) {
+                if (i%j == 0) {
+                    divisorList.add(j)
+                    if (i/j != j) {
+                        divisorList.add(j)
+                    }
+                }
             }
             divisorNumList.add(divisorList.count())
             divisorList.clear()
         }
-    
+        
         for (i in 0 until divisorNumList.size) {
             if (divisorNumList[i] > limit) {
                 divisorNumList[i] = power
