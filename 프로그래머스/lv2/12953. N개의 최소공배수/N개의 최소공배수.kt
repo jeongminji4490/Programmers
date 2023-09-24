256cc062e359aa829d753e3f2a4d10d071510fc8
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-09-24 15:53:58 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.kt
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.kt
@@ -1,18 +1,35 @@
 class Solution {
     fun solution(arr: IntArray): Int {
-        var answer = arr[0]
-        arr.forEach{ answer = lcm(answer, it) }
+        var answer = 0
+        
+        if (arr.size == 1) {
+            return arr[0]
+        } else if (arr.size == 2) {
+            return lcd(arr[0], arr[1])
+        } else {
+            var n1 = arr[arr.size-1]
+            var n2 = arr[arr.size-2]
+        
+            var lcd = lcd(n1, n2)
+            answer = lcd
+            
+            for (i in arr.size-3 downTo 0) {
+                while (true) {
+                    if (answer%arr[i] == 0) {
+                        lcd = answer
+                        break
+                    }
+                    answer += lcd 
+                }
+            }
+        
+        }
+        
         return answer  
     }
-        
-    fun lcm(a:Int, b:Int) = a * b / gcd(a, b)
     
-    fun gcd(a:Int, b:Int):Int{
-        return if (a < b){
-            if (a == 0) b else gcd(a, b % a)
-        }
-        else{
-            if (b == 0) a else gcd(b, a % b)
-        }
-    }
+    fun lcd(a: Int, b: Int) = (a * b)/gcd(a, b)
+    
+    fun gcd(a: Int, b: Int): Int = if (b != 0) gcd(b, a%b) else a
+        
 }
